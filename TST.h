af50db9668f68c7ccafde7886d391ff1a708d06f
Giacomo Donati <donatigiacomo91@gmail.com>
2016-02-08 12:11:29 +0100

    basic tst impl that compile

--- TST.h
+++ TST.h
@@ -5,4 +5,92 @@
 #ifndef AUTOCOMPLETE_TST_H
 #define AUTOCOMPLETE_TST_H
 
+#include <vector>
+
+namespace tst {
+
+    template<typename A, typename P, typename D>
+    class Node {
+    public:
+        A character;
+        P left, middle, right;
+        D index; // 0 if no word end in the node
+
+        Node(A c) {
+            character = *c;
+        }
+    };
+
+    template<typename A, typename P, typename D>
+    class Tree {
+        std::vector<Node<A,P,D>*> vec;
+
+        P insert(P, const A*, const D);
+        void create(const std::vector<A*>&, P, P);
+
+    public:
+        Tree(const std::vector<A*>& dic){
+
+            if (dic.size() <= 1) {
+                return ;
+            }
+
+            D root_index = dic.size()/2;
+            insert(0, dic[root_index], root_index);
+
+            create(dic, 1, root_index);
+            create(dic, root_index+1, dic.size());
+
+        }
+    };
+
+    template<typename A, typename P, typename D>
+    P Tree<A,P,D>::insert(P node_index, const A* word, const D dic_index) {
+
+        Node<A,P,D>* node;
+
+        if (node_index == 0) {
+            node = new Node<A,P,D>(word);
+            node->left = 0;
+            node->middle = 0;
+            node->right = 0;
+
+            vec.push_back(node);
+            node_index = vec.size()-1;
+        } else {
+            node = vec[node_index];
+        }
+
+        if (*word < node->character) {
+            node->left = insert(node->left, *word, dic_index);
+        }
+        else if (*word == node->character) {
+
+            if (*word != 0) {
+                node->middle = insert(node->middle, ++word, dic_index);
+            } else {
+                node->index = dic_index;
+            }
+
+        } else {
+            node->right = insert(node->right, *word, dic_index);
+        }
+
+        return node_index;
+
+    };
+
+    template<typename A, typename P, typename D>
+    void Tree<A,P,D>::create(const std::vector<A*>& dic, P min, P max) {
+
+        if (min < max) {
+            P mid = (min+max)/2;
+            insert(1, dic[mid], mid);
+            create(dic, min, mid);
+            create(dic, mid+1, max);
+        }
+    }
+
+}
+
 #endif //AUTOCOMPLETE_TST_H
--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <algorithm>
 #include <vector>
 
-#include "Trie.h"
+#include "TST.h"
 
 using namespace std;
 
@@ -18,26 +18,28 @@ void gen_random(char *s, const int len) {
 
 int main() {
 
-    BasicTrie<char,unsigned int> trie;
-    char str[11];
+//    BasicTrie<char,unsigned int> trie;
+//    char str[11];
+//
+//    long dic_size = 0;
+//    for(int i=1; i < 1000; i++) {
+//        int len = rand() % 8 + 3;
+//        dic_size = dic_size + len;
+//        gen_random(str,len);
+//        trie.insert(str,len,i);
+//    }
+//    // convert to bit
+//    dic_size = dic_size*8;
+//
+//    long trie_size = 0;
+//    vector<BasicTrieNode<char,unsigned int>*> vec = trie.getTrieVector();
+//    for (int j = 0; j < vec.size(); ++j) {
+//        trie_size = trie_size + vec[j]->get_size();
+//    }
+//
+//
+//    cout << dic_size << endl << trie_size << endl << vec.size() << endl;
 
-    long dic_size = 0;
-    for(int i=1; i < 1000; i++) {
-        int len = rand() % 8 + 3;
-        dic_size = dic_size + len;
-        gen_random(str,len);
-        trie.insert(str,len,i);
-    }
-    // convert to bit
-    dic_size = dic_size*8;
 
-    long trie_size = 0;
-    vector<BasicTrieNode<char,unsigned int>*> vec = trie.getTrieVector();
-    for (int j = 0; j < vec.size(); ++j) {
-        trie_size = trie_size + vec[j]->get_size();
-    }
-
-
-    cout << dic_size << endl << trie_size << endl << vec.size() << endl;
     return 0;
 }
